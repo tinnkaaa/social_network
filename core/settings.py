841910a37d232f0712f43db90dc1f8de9d5237d5
tinnkaaa <geletejkristina4@gmail.com>
2026-03-10 21:51:02 +0100

    fix google oauth

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['social-network-yngr.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1']
 
 
