bbcb51d87f0d79305c00d0ef69e94cfdc702fc93
tinnkaaa <geletejkristina4@gmail.com>
2026-03-11 18:28:13 +0100

    update chat models

--- chat/models.py
+++ chat/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from auth_system.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class ChatParticipant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_participations', verbose_name='Користувач')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='chat_participations', verbose_name='Користувач')
     chat = models.ForeignKey('Chat', on_delete=models.CASCADE, related_name='chat_participants', verbose_name='Чат')
     is_admin = models.BooleanField(default=False, verbose_name='Адміністратор')
 
 class Chat(models.Model):
     is_group = models.BooleanField(default=False, verbose_name='Груповий чат')
     title = models.CharField(max_length=100, blank=True, verbose_name='Назва')
-    participants = models.ManyToManyField(User, through='ChatParticipant', related_name='chats', verbose_name='Учасники')
+    participants = models.ManyToManyField(get_user_model(), through='ChatParticipant', related_name='chats', verbose_name='Учасники')
     updated_at = models.DateTimeField(auto_now=True)
 
     def unread_count_for(self, user):
@@ -20,7 +20,7 @@ class Chat(models.Model):
 
 class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages', verbose_name='Чат')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Відправник')
+    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='Відправник')
     text = models.TextField(verbose_name='Текст повідомлення')
     is_read = models.BooleanField(default=False, verbose_name='Прочитано')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата створення')
