2658321b0229819d3e06fd676977ffaf72b0a130
tinnkaaa <geletejkristina4@gmail.com>
2026-03-11 18:14:23 +0100

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -e
 
-# Встановлюємо налаштування Django
-export DJANGO_SETTINGS_MODULE=core.settings
+echo "Waiting for database and environment to be ready..."
 
+# Міграції
 echo "Apply migrations..."
 python manage.py migrate --settings=core.settings --noinput
 
+# Static файли
 echo "Collect static files..."
 python manage.py collectstatic --settings=core.settings --noinput
 
+# Створення суперюзера
 echo "Create superuser if not exists..."
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_PASSWORD=12345
 export DJANGO_SUPERUSER_EMAIL=admin@example.com
-
-# --noinput + || true щоб не падало, якщо юзер вже існує
 python manage.py createsuperuser --settings=core.settings --noinput || true
 
+# Запуск Daphne на динамічному порті Render
 echo "Starting Daphne..."
-# Використовуємо динамічний порт Render
 exec daphne -b 0.0.0.0 -p ${PORT:-8000} core.asgi:application
