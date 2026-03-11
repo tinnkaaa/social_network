87c4063bddc95d0ad1b337b1c45008ce50ecf214
tinnkaaa <geletejkristina4@gmail.com>
2026-03-11 17:56:08 +0100

    update asgi

--- core/asgi.py
+++ core/asgi.py
@@ -1,16 +1,16 @@
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # не імпортуй models тут!
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,      # HTTP через Django
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)  # WS через Channels
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
